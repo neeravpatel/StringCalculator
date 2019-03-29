233bccdabc8de7779e8487305dbc78429807dfd1
Neerav Patel <NPATEL@eina.evrazinc.ad>
2019-03-29 17:04:18 -0600

    Initial commit

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculatorNamespace
+{
+    public class StringCalculator
+    {
+        static void Main(string[] args)
+        {
+
+            string number = "";
+            List<string> numbers = new List<string> { };
+            do
+            {
+                number = Console.ReadLine()+Environment.NewLine;
+                numbers.Add(number);
+
+            } while (number != Environment.NewLine);
+
+            StringCalculator sc = new StringCalculator();
+            int result = sc.Add(numbers);
+
+            Console.WriteLine(result.ToString());
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+
+        public int Add(List<string> numbers)
+        {
+            int result = 0;
+            /* ',' default delimiter */
+            string[] tempdelimiter = {","};
+            string[] delimiter = { "," };
+            /* // control code in beginning to define custom delimiter */
+            string controlcode = "//"; 
+
+            foreach (string str in numbers)
+            {
+                if (str != "\r\n")
+                {
+                    if (str.StartsWith(controlcode))
+                    {
+                        string tstr = str.Remove(0, 2).Remove(str.Length - 4, 2);
+                        delimiter = tstr.Split(tempdelimiter, StringSplitOptions.RemoveEmptyEntries);
+                        continue;
+                    }
+
+                    string[] num = str.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string item in num)
+                    {
+                        if (item != "\r\n")
+                        {
+                            try
+                            {
+                                int i = int.Parse(item);
+
+                                if (i > 0)
+                                {
+                                    if (i <= 1000)
+                                        result += i;
+                                }
+                                else
+                                    throw new System.ArgumentException("Negatives not allowed " + item);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Exception encountered: " + ex.Message);
+
+                                // Keep the console window open in debug mode.
+                                Console.WriteLine("Press any key to exit.");
+                                Console.ReadKey();
+                                Environment.Exit(0);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return result;
+        }
+
+    }
+}
---
+++ StringCalculatorUnitTest/StringCalculatorUnitTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using StringCalculatorNamespace;
+
+namespace StringCalculatorUnitTest
+{
+    [TestClass]
+    public class StringCalculatorUnitTest
+    {
+        StringCalculator obj = new StringCalculator();
+
+        [TestMethod]
+        public void TestAddSimple()
+        {
+
+            List<string> testnumbers = new List<string> { "1,2,5" };
+            int result = obj.Add(testnumbers);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void TestAddNewLine()
+        {
+            List<string> testnumbers1 = new List<string> { "1\r\n", ",2,3" };
+            int result1 = obj.Add(testnumbers1);
+            Assert.AreEqual(6, result1);
+
+            List<string> testnumbers2 = new List<string> { "1,\r\n", "2,4" };
+            int result2 = obj.Add(testnumbers2);
+            Assert.AreEqual(7, result2);
+        }
+
+        [TestMethod]
+        public void TestAddCustomDelimiter()
+        {
+            List<string> testnumbers = new List<string> { "//;\r\n", "1;3;4" };
+            int result = obj.Add(testnumbers);
+            Assert.AreEqual(8, result);
+
+            List<string> testnumbers2 = new List<string> { "//$\r\n", "1$2$3" };
+            int result2 = obj.Add(testnumbers2);
+            Assert.AreEqual(6, result2);
+
+            List<string> testnumbers3 = new List<string> { "//@\r\n", "2@3@8" };
+            int result3 = obj.Add(testnumbers3);
+            Assert.AreEqual(13, result3);
+        }
+
+        [TestMethod]
+        public void TestAddIgnoreLargeThan1000()
+        {
+            List<string> testnumbers = new List<string> { "1001,3" };
+            int result = obj.Add(testnumbers);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestAddDelimiterArbitraryLength()
+        {
+            List<string> testnumbers = new List<string> { "//***\r\n", "1***2***3" };
+            int result = obj.Add(testnumbers);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void TestAddMultipleDelimiter()
+        {
+            List<string> testnumbers = new List<string> { "//$,@\r\n", "1$2@3" };
+            int result = obj.Add(testnumbers);
+            Assert.AreEqual(6, result);
+        }
+
+
+        [TestMethod]
+        public void TestAddMultipleDelimiterArbitraryLength()
+        {
+            List<string> testnumbers1 = new List<string> { "//**,@*\r\n", "1**2@*3" };
+            int result1 = obj.Add(testnumbers1);
+            Assert.AreEqual(6, result1);
+
+            List<string> testnumbers2 = new List<string> { "//**,@\r\n", "3**3@3" };
+            int result2 = obj.Add(testnumbers2);
+            Assert.AreEqual(9, result2);
+
+        }
+
+    }
+}
